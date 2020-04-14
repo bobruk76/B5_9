5fd95bccd8cc74d20a9341354d1667b6768a62c2
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-04-14 23:46:05 +0300

    Update b5_9.py

--- b5_9.py
+++ b5_9.py
@@ -1,47 +1,30 @@
-
-
-
-
-def func_run(func):
-    def wrap(param):
-        for _ in range(NUM_RUNS):
-            dt = func(param)
-        return True
-    return wrap
-
-
-NUM_RUNS = 10
-
-#@avg_wrap(NUM_RUNS)
-
-class sec_mer():
-    import time
-
-    @func_run
-    def __init__(self,NUM_RUNS):
+class Stopwatch:
+    def __init__(self, func, NUM_RUNS = 10):
+        import time
+        self._NUM_RUNS = NUM_RUNS
         self._t0=time.time()
 
+        for _ in range(NUM_RUNS):
+            dt = func()
 
-
-class Fib:
-    def __init__(self, len=100):
-        self._list = [0, 1]
-        self._len = len
-        n = 1
-        while n < len-1:
-            self._list.append(self._list[n]+self._list[n-1])
-            n += 1
-
-    def __len__(self):
-        return len(self._list)
-
-    def __getitem__(self, position):
-        return str(self._list[position])
+        self._t1=time.time()
+        self._time = self._t1 - self._t0
 
     def __str__(self):
-        return ','.join(map(str,self._list))
+        return str(self._time)
 
+def fib_list(num):
+    fib_list = [0, 1]
+    n = 1
+    while n < num-1:
+        fib_list.append(fib_list[n]+fib_list[n-1])
+        n += 1
+    return ','.join([str(item) for item in fib_list])
 
-fib_list = Fib(len=100)
-print(fib_list)
-print(fib_list[10])
+def main():
+    NUM_RUNS = 10
+ #   fib_stopwatch = Stopwatch()
+    print(fib_list())
+
+if __name__ == "__main__":
+    main()
