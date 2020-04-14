20110efe7a4247c73623a566ddb970687683b274
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-04-14 16:05:01 +0300

    end class fib

--- b5_9.py
+++ b5_9.py
@@ -1,29 +1,47 @@
-import time
 
-def avg_wrap(NUM_RUNS):
-    def avg_time(func):
-        def wrap(param):
-            avg_t = 0.0
-            for _ in range(NUM_RUNS):
-                t0 = time.time()
-                dt = func(param)
-                t1 = time.time()
-                avg_t += (t1-t0)
-            avg_t /= NUM_RUNS
-            return avg_t
-        return wrap
-    return avg_time
+
+
+
+def func_run(func):
+    def wrap(param):
+        for _ in range(NUM_RUNS):
+            dt = func(param)
+        return True
+    return wrap
+
 
 NUM_RUNS = 10
-@avg_wrap(NUM_RUNS)
-def fib_list(num):
-    fib_list = [0, 1]
-    n = 1
-    while n < num-1:
-        fib_list.append(fib_list[n]+fib_list[n-1])
-        n += 1
-    return ','.join([str(item) for item in fib_list])
 
-print(fib_list(50000))
+#@avg_wrap(NUM_RUNS)
+
+class sec_mer():
+    import time
+
+    @func_run
+    def __init__(self,NUM_RUNS):
+        self._t0=time.time()
 
 
+
+class Fib:
+    def __init__(self, len=100):
+        self._list = [0, 1]
+        self._len = len
+        n = 1
+        while n < len-1:
+            self._list.append(self._list[n]+self._list[n-1])
+            n += 1
+
+    def __len__(self):
+        return len(self._list)
+
+    def __getitem__(self, position):
+        return str(self._list[position])
+
+    def __str__(self):
+        return ','.join(map(str,self._list))
+
+
+fib_list = Fib(len=100)
+print(fib_list)
+print(fib_list[10])
