494ca8c815bea02cd610a89ab6f5a2a3bff63766
Vladimir Poliakov <60501887+bobruk76@users.noreply.github.com>
2020-04-15 10:55:58 +0300

    end task

--- b5_9.py
+++ b5_9.py
@@ -1,33 +1,35 @@
 class Stopwatch:
-    def __init__(self, func, NUM_RUNS = 10):
-        def wrapper(param):
-            import time
-            self._NUM_RUNS = NUM_RUNS
-            self._t0=time.time()
+    def __init__(self, NUM_RUNS):
+        self._NUM_RUNS = NUM_RUNS
 
-            for _ in range(NUM_RUNS):
-                print(func(param))
-
-            self._t1=time.time()
-            self._time = self._t1 - self._t0
-            return wrapper
+    def __call__(self, fn):
+        import time
+        import statistics as st
+        def wrapp(*args):
+            self._time_list = []
+            for _ in range(self._NUM_RUNS):
+                _t0 = time.time()
+                _f = fn(*args)
+                self._time_list.append(time.time() - _t0)
+            self._avg_time = st.mean(self._time_list)
+            self._fn_result = _f
+            return "{}\n Выполнение заняло {} секунд\n Время выполнения каждого шага:{}".format(self._fn_result, '%.5f' % self._avg_time, ','.join([str(item) for item in self._time_list]))
+        return wrapp
 
     def __str__(self):
         return str(self._time)
 
-@Stopwatch
-def fib_list(num=100):
+NUM_RUNS = 100
+@Stopwatch(NUM_RUNS)
+def fib_list(num=5000):
     fib_list = [0, 1]
-    n = 1
-    while n < num-1:
+    for n in range(1,num):
         fib_list.append(fib_list[n]+fib_list[n-1])
-        n += 1
-    return ','.join([str(item) for item in fib_list])
+    return 'Сумма первых {} членов последовательности Фиббоначи равна {}'.format(num, sum(fib_list))
 
 def main():
-    NUM_RUNS = 10
-
-    print(fib_list())
+    num = 500
+    print(fib_list(num))
 
 if __name__ == "__main__":
     main()
